f00ed6446f39aece20423aba87385832e2d3eb9b
Alexey Vasilev <40358104+perpetua1g0d@users.noreply.github.com>
2021-08-04 14:10:19 +0300

    Create FastScanner.java

---
+++ FastScanner.java
@@ -0,0 +1,34 @@
+class FastScanner {
+    public BufferedReader reader;
+    public StringTokenizer tokenizer;
+    public FastScanner() {
+        reader = new BufferedReader(new InputStreamReader(System.in), 32768);
+        tokenizer = null;
+    }
+    public String next() {
+        while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+            try {
+                tokenizer = new StringTokenizer(reader.readLine());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return tokenizer.nextToken();
+    }
+    public int nextInt() {
+        return Integer.parseInt(next());
+    }
+    public long nextLong() {
+        return Long.parseLong(next());
+    }
+    public double nextDouble() {
+        return Double.parseDouble(next());
+    }
+    public String nextLine() {
+        try {
+            return reader.readLine();
+        } catch(IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
